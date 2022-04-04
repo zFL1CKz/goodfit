9e0b01cfaf32cc788d24827fc5431cf8777fcec7
zFL1CKz <dimakukharev2002@yandex.ru>
2022-04-04 18:33:44 +0300

    added ActivityCard

--- client/src/components/cards/ActivityCard.jsx
+++ client/src/components/cards/ActivityCard.jsx
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import activityImg from '../../img/icons/activity_check.svg'
 import './ActivityCard.css'
-
 export const ActivityCard = ({ item }) => {
+  const ref = useRef(null)
+
+  function handleActive() {
+    clearActivity()
+    ref.current.classList.add('active')
+  }
+  function clearActivity() {
+    let arr = document.querySelectorAll('.activity__item')
+    for (const item of arr) {
+      if (item.classList.contains('active')) {
+        item.classList.remove('active')
+        break
+      }
+    }
+  }
+  function firstActive() {
+    setTimeout(() => {
+      document.querySelector('.activity__item').classList.add('active')
+    }, 0)
+  }
+  firstActive()
+
   return (
-    <div className='activity__item' onClick={() => {}}>
+    <div className='activity__item' ref={ref} onClick={handleActive}>
       <div className='activity__title'>{item.name}</div>
       <div className='activity__group'>
         <div className='activity__desc'>{item.desc}</div>
