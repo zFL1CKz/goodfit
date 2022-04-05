4de830a3058ee2be1fba4b0040b2e2715aacd46d
zFL1CKz <dimakukharev2002@yandex.ru>
2022-04-05 20:17:45 +0300

    error css module

--- client/src/pages/Auth/RegisterPage.jsx
+++ client/src/pages/Auth/RegisterPage.jsx
@@ -11,6 +11,7 @@ import InputMask from 'react-input-mask'
 
 import authModule from '../../modules/Auth.module.css'
 import profileModule from '../../modules/Profile.module.css'
+import errorModule from '../../modules/Error.module.css'
 import screenImg5 from '../../img/startScreens/5.jpg'
 
 export const RegisterPage = () => {
@@ -18,13 +19,20 @@ export const RegisterPage = () => {
   const { request } = useHttp()
 
   // const [modalActive, setModalActive] = useState(false)
+  const [error, setError] = useState('')
   const [isReady, setIsReady] = useState(true)
   const [isChecked, setIsChecked] = useState(true)
   const [screen, setScreen] = useState(7)
   const [genderActive, setGenderActive] = useState(true)
   const [validForm, setValidForm] = useState(false)
   const [activities, setActivities] = useState([])
-  const [resultDate, setResultDate] = useState(new Date(2002, 10, 18).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }))
+  const [resultDate, setResultDate] = useState(
+    new Date(2002, 10, 18).toLocaleDateString('en-US', {
+      month: 'short',
+      day: 'numeric',
+      year: 'numeric',
+    })
+  )
   const [form, setForm] = useState({
     email: '',
     password: '',
@@ -77,8 +85,17 @@ export const RegisterPage = () => {
   useEffect(() => {
     if (info.birth.indexOf('_') === -1 && info.birth.split('').length === 10) {
       let date = info.birth.split('.')
-      if (Math.round(new Date(`${date[1]}.${date[0]}.${date[2]}`).getTime() / 1000.0) <= Math.round(new Date().getTime() / 1000.0)) {
-        setResultDate(new Date(`${date[1]}.${date[0]}.${date[2]}`).toLocaleDateString('en-US', { month: 'short', day: 'numeric', year: 'numeric' }))
+      if (
+        Math.round(
+          new Date(`${date[1]}.${date[0]}.${date[2]}`).getTime() / 1000.0
+        ) <= Math.round(new Date().getTime() / 1000.0)
+      ) {
+        setResultDate(
+          new Date(`${date[1]}.${date[0]}.${date[2]}`).toLocaleDateString(
+            'en-US',
+            { month: 'short', day: 'numeric', year: 'numeric' }
+          )
+        )
       }
     }
   }, [info.birth])
@@ -88,8 +105,22 @@ export const RegisterPage = () => {
   } else {
     return (
       <div>
-        {screen === 1 && <StartScreen screen={screen} title='GoodFIT' desc='Быть здоровым - это все, отсутствие здоровья - это ничто.' setNewScreen={setNewScreen} />}
-        {screen === 2 && <StartScreen screen={screen} title='Be stronger' desc='Занимайся спортом от 30-ти минут в день.' setNewScreen={setNewScreen} />}
+        {screen === 1 && (
+          <StartScreen
+            screen={screen}
+            title='GoodFIT'
+            desc='Быть здоровым - это все, отсутствие здоровья - это ничто.'
+            setNewScreen={setNewScreen}
+          />
+        )}
+        {screen === 2 && (
+          <StartScreen
+            screen={screen}
+            title='Be stronger'
+            desc='Занимайся спортом от 30-ти минут в день.'
+            setNewScreen={setNewScreen}
+          />
+        )}
         {screen === 3 && (
           <StartScreen
             screen={screen}
@@ -104,14 +135,34 @@ export const RegisterPage = () => {
             <TopCircle text='Регистрация' />
             <div className='container'>
               <div className='auth'>
-                <input type='text' className={authModule.auth__input} name='email' onChange={changeHandler} placeholder='Email' />
-                <input type='password' className={authModule.auth__input} name='password' onChange={changeHandler} placeholder='Пароль' />
-                <input type='password' className={authModule.auth__input} name='confirmPassword' onChange={changeHandler} placeholder='Повторите пароль' />
+                <input
+                  type='text'
+                  className={authModule.auth__input}
+                  name='email'
+                  onChange={changeHandler}
+                  placeholder='Email'
+                />
+                <input
+                  type='password'
+                  className={authModule.auth__input}
+                  name='password'
+                  onChange={changeHandler}
+                  placeholder='Пароль'
+                />
+                <input
+                  type='password'
+                  className={authModule.auth__input}
+                  name='confirmPassword'
+                  onChange={changeHandler}
+                  placeholder='Повторите пароль'
+                />
                 <div className={authModule.authBottom}>
                   <Link to='/login' className={authModule.auth__text}>
                     Уже есть аккаунт?
                   </Link>
-                  <button className={authModule.auth__btn} onClick={() => checkForm()}>
+                  <button
+                    className={authModule.auth__btn}
+                    onClick={() => checkForm()}>
                     Регистрация
                   </button>
                 </div>
@@ -127,13 +178,21 @@ export const RegisterPage = () => {
 
             <div className={authModule.auth__footer_circle}>
               <div>
-                <div className={authModule.auth__footer_title}>Добро пожаловать в приложение GoodFIT</div>
-                <div className={authModule.auth__footer_desc}>Индивидуальные тренировки помогут вам набраться сил, прийти в лучшую форму и вести здоровый образ жизни</div>
-                <button onClick={() => setScreen(6)} className={authModule.auth__footer_btn}>
+                <div className={authModule.auth__footer_title}>
+                  Добро пожаловать в приложение GoodFIT
+                </div>
+                <div className={authModule.auth__footer_desc}>
+                  Индивидуальные тренировки помогут вам набраться сил, прийти в
+                  лучшую форму и вести здоровый образ жизни
+                </div>
+                <button
+                  onClick={() => setScreen(6)}
+                  className={authModule.auth__footer_btn}>
                   Начать
                 </button>
                 <div className={authModule.auth__footer_circleBtns}>
-                  <div className={`${authModule.auth__footer_circleBtn} ${authModule.active}`}></div>
+                  <div
+                    className={`${authModule.auth__footer_circleBtn} ${authModule.active}`}></div>
                   <div className={authModule.auth__footer_circleBtn}></div>
                   <div className={authModule.auth__footer_circleBtn}></div>
                 </div>
@@ -146,7 +205,9 @@ export const RegisterPage = () => {
           <div>
             <TopCircle text={'Step 2 of 3'} />
             <div className='container'>
-              <div className={`${authModule.auth__title} ${authModule.mb60}`}>Выберите степень физической активности</div>
+              <div className={`${authModule.auth__title} ${authModule.mb60}`}>
+                Выберите степень физической активности
+              </div>
               <div className={authModule.auth__subtitle}></div>
               <div className='activity'>
                 {activities.map((item, index) => {
@@ -155,13 +216,16 @@ export const RegisterPage = () => {
               </div>
               <div className={authModule.absolute}>
                 <div className='container'>
-                  <div className={authModule.auth__btn} onClick={() => setScreen(7)}>
+                  <div
+                    className={authModule.auth__btn}
+                    onClick={() => setScreen(7)}>
                     Далее
                   </div>
                 </div>
                 <div className={authModule.auth__circles}>
                   <div className={authModule.auth__circle}></div>
-                  <div className={`${authModule.auth__circle} ${authModule.active}`}></div>
+                  <div
+                    className={`${authModule.auth__circle} ${authModule.active}`}></div>
                   <div className={authModule.auth__circle}></div>
                 </div>
               </div>
@@ -174,20 +238,44 @@ export const RegisterPage = () => {
             <TopCircle text={'Step 3 of 3'} />
             <div className='container'>
               <div className={authModule.auth__title}>Личные данные</div>
-              <div className={authModule.auth__subtitle}>Дайте нам знать о себе, чтобы ускорить результат, привести себя в форму с помощью вашего личного плана тренировок!</div>
-
-              <div className={profileModule.profile__group}>
-                <label htmlFor='calcCheck' className={profileModule.profile__labelCheck}>
-                  <div className={profileModule.profile__title}>Расчет калорий</div>
-                  <div className={profileModule.profile__subtitle}>Включить персональный расчет калорий</div>
-                </label>
-                <input type='checkbox' id='calcCheck' className={profileModule.profile__inputCheck} checked={isChecked} onClick={() => setIsChecked(!isChecked)} />
+              <div className={authModule.auth__subtitle}>
+                Дайте нам знать о себе, чтобы ускорить результат, привести себя
+                в форму с помощью вашего личного плана тренировок!
               </div>
 
               <div className={profileModule.profile__group}>
-                <div className={profileModule.profile__title}>Дата рождения</div>
+                <label
+                  htmlFor='calcCheck'
+                  className={profileModule.profile__labelCheck}>
+                  <div className={profileModule.profile__title}>
+                    Расчет калорий
+                  </div>
+                  <div className={profileModule.profile__subtitle}>
+                    Включить персональный расчет калорий
+                  </div>
+                </label>
+                <input
+                  type='checkbox'
+                  id='calcCheck'
+                  className={profileModule.profile__inputCheck}
+                  checked={isChecked}
+                  onClick={() => setIsChecked(!isChecked)}
+                />
+              </div>
+
+              <div className={profileModule.profile__group}>
+                <div className={profileModule.profile__title}>
+                  Дата рождения
+                </div>
                 {resultDate === undefined ? (
-                  <InputMask mask='99.99.9999' maskPlaceholder='' onChange={infoHandler} id='birth' name='birth' className={profileModule.profile__input} />
+                  <InputMask
+                    mask='99.99.9999'
+                    maskPlaceholder=''
+                    onChange={infoHandler}
+                    id='birth'
+                    name='birth'
+                    className={profileModule.profile__input}
+                  />
                 ) : (
                   <span
                     className={profileModule.profile__input}
@@ -205,7 +293,15 @@ export const RegisterPage = () => {
               <div className={profileModule.profile__group}>
                 <div className={profileModule.profile__title}>Рост</div>
                 <div>
-                  <InputMask mask='999' maskPlaceholder='' placeholder='0' id='height' onChange={infoHandler} name='height' className={profileModule.profile__input} />
+                  <InputMask
+                    mask='999'
+                    maskPlaceholder=''
+                    placeholder='0'
+                    id='height'
+                    onChange={infoHandler}
+                    name='height'
+                    className={profileModule.profile__input}
+                  />
                   <label htmlFor='height' className={profileModule.label__span}>
                     &nbsp;cm
                   </label>
@@ -214,7 +310,15 @@ export const RegisterPage = () => {
               <div className={profileModule.profile__group}>
                 <div className={profileModule.profile__title}>Вес</div>
                 <div>
-                  <InputMask mask='999' maskPlaceholder='' placeholder='0' id='weight' onChange={infoHandler} name='weight' className={profileModule.profile__input} />
+                  <InputMask
+                    mask='999'
+                    maskPlaceholder=''
+                    placeholder='0'
+                    id='weight'
+                    onChange={infoHandler}
+                    name='weight'
+                    className={profileModule.profile__input}
+                  />
                   <label htmlFor='weight' className={profileModule.label__span}>
                     &nbsp;kg
                   </label>
@@ -224,10 +328,18 @@ export const RegisterPage = () => {
               <div className={profileModule.profile__group}>
                 <div className={profileModule.profile__title}>Пол</div>
                 <div className={profileModule.gender__wrapper}>
-                  <div className={`${profileModule.gender__item} ${genderActive && profileModule.active}}`} onClick={() => setGenderActive(true)}>
+                  <div
+                    className={`${profileModule.gender__item} ${
+                      genderActive && profileModule.active
+                    }`}
+                    onClick={() => setGenderActive(true)}>
                     Муж.
                   </div>
-                  <div className={`${profileModule.gender__item} ${!genderActive && profileModule.active}}`} onClick={() => setGenderActive(false)}>
+                  <div
+                    className={`${profileModule.gender__item} ${
+                      !genderActive && profileModule.active
+                    }`}
+                    onClick={() => setGenderActive(false)}>
                     Жен.
                   </div>
                 </div>
@@ -235,14 +347,18 @@ export const RegisterPage = () => {
 
               <div className={authModule.absolute}>
                 <div className='container'>
-                  <div className={authModule.auth__btn} onClick={() => setScreen(7)}>
+                  <div className={errorModule.error__text}>{error}</div>
+                  <div
+                    className={authModule.auth__btn}
+                    onClick={() => setScreen(7)}>
                     Далее
                   </div>
                 </div>
                 <div className={authModule.auth__circles}>
                   <div className={authModule.auth__circle}></div>
                   <div className={authModule.auth__circle}></div>
-                  <div className={`${authModule.auth__circle} ${authModule.active}`}></div>
+                  <div
+                    className={`${authModule.auth__circle} ${authModule.active}`}></div>
                 </div>
               </div>
             </div>
